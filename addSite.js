85f49d770580914dc935945c6c32eb6fad6de19d
gomathinayagamA <66545050+gomathinayagamA@users.noreply.github.com>
2021-05-26 11:24:29 +0530

    second

--- addSite.js
+++
@@ -1,9 +0,0 @@
-var AddBtn = document.getElementById("Add");
-AddBtn.addEventListener("click",saveValues);
-function saveValues()
-{
-   var site = document.getElementById("site_name").value;
-   var time = document.getElementById("site_time").value;
-     window.localStorage.setItem(JSON.stringify(site),Number(time));
-     alert("Stored");
-}
